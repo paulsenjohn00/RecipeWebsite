2d3ff361efce697555d056dc23a55f0f7b0ce3b6
ajp86 <paulsenjohn00@gmail.com>
2022-12-28 11:46:39 -0700

    databasechanges

--- recipewebsite/settings.py
+++ recipewebsite/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'recipewebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resume-recipe',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Apau7101!',
+        'HOST' : 'localhost'
     }
 }
 
