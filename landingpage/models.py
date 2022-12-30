b3c938e6271023671f25836b8355189cd2d68198
ajp86 <paulsenjohn00@gmail.com>
2022-12-30 15:37:59 -0700

    database

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    year = models.IntegerField(default=0)
+    color = models.CharField(max_length=20)
+    mileage = models.IntegerField(default=0)
+    cleanTitle = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return '%s %s %s' %(self.make,self.model,str(self.year))
