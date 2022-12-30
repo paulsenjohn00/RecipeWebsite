3d5a555f56ebc20083d28f08322046bfccde176e
ajp86 <paulsenjohn00@gmail.com>
2022-12-30 15:00:13 -0700

    allowedhost

--- recipewebsite/settings.py
+++ recipewebsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://johnnypaulsen.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://johnnypaulsen.up.railway.app']
 
 # Application definition
